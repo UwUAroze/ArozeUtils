b7a50864b501650aa55faa51ad4c7abc80c65f61
UwUAroze <admin@aroze.me>
2023-01-22 18:16:07 +0000

    Custom command executor

---
+++ src/main/kotlin/minecraft/command/BaseCommand.kt
@@ -0,0 +1,35 @@
+package minecraft.command
+
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
+import org.bukkit.command.CommandMap
+import org.bukkit.command.CommandSender
+
+abstract class BaseCommand(val command: String) : Command(command) {
+
+    val commandInfo = this::class.java.getAnnotation(CommandInfo::class.java)
+
+    init {
+        this.aliases = commandInfo.aliases.toList()
+        this.description = commandInfo.description
+
+        try {
+            val mapField = Bukkit.getServer().javaClass.getDeclaredField("commandMap")
+            mapField.isAccessible = true
+            val map = mapField.get(Bukkit.getServer()) as CommandMap
+            map.register(command, this)
+        }
+
+        catch(_: NoSuchFieldException) { }
+        catch(_: IllegalAccessException) { }
+
+    }
+
+    abstract fun onCommand(sender: CommandSender, label: String, args: Array<out String>)
+
+    override fun execute(sender: CommandSender, commandLabel: String, args: Array<out String>): Boolean {
+
+        return true
+
+    }
+}
---
+++ src/main/kotlin/minecraft/command/CommandInfo.kt
@@ -0,0 +1,30 @@
+package minecraft.command
+
+import org.bukkit.command.CommandSender
+import org.bukkit.command.ConsoleCommandSender
+import org.bukkit.entity.Player
+
+annotation class CommandInfo(
+
+    val name: String,
+    val description: String = "",
+    val aliases: Array<String> = [],
+
+    val initialPermission: String = "",
+    val initialPermissionMessage: String = "",
+
+    val allowedSenders: CommandSenders = CommandSenders.ALL,
+    val disallowedSenderMessage: String = ""
+
+)
+
+enum class CommandSenders {
+    PLAYER, CONSOLE, ALL;
+
+    fun isAcceptable(sender: CommandSender) = when (this) {
+        PLAYER -> sender is Player
+        CONSOLE -> sender !is Player
+        ALL -> true
+    }
+
+}
