6d22b73d1b7d31fde789356ed69caed1688a2ac1
UwUAroze <admin@aroze.me>
2023-01-22 01:13:48 +0000

    Added some item utils

---
+++ src/main/kotlin/minecraft/ItemUtils.kt
@@ -0,0 +1,17 @@
+package minecraft
+
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.meta.Damageable
+
+fun ItemStack.repair() : Boolean {
+    if (!this.isDamaged()) return false
+    val meta = this.itemMeta as Damageable
+    meta.damage = 0
+    this.itemMeta = meta
+    return true
+}
+
+fun ItemStack.isDamaged() : Boolean {
+    if (this.itemMeta !is Damageable) return false
+    return ((this.itemMeta as Damageable).damage != 0)
+}
