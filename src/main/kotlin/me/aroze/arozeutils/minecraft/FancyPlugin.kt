42d5ea5b1013fb9622fe37e70522ce65cc5958fe
UwUAroze <admin@aroze.me>
2023-01-29 23:09:58 +0000

    grammar

--- src/main/kotlin/me/aroze/arozeutils/minecraft/FancyPlugin.kt
+++ src/main/kotlin/me/aroze/arozeutils/minecraft/FancyPlugin.kt
@@ -10,9 +10,9 @@ private lateinit var pluginInstance: FancyPlugin
 abstract class FancyPlugin: JavaPlugin() {
 
     open fun onPluginLoad() {/*
-        We override and use onLoad() to access the plugin instance, needed for several features
-        Because of this overriding, nothing in your plugin's onLoad() will be run
-        To fix this, you should override this - onPluginLoad() - instead, and put your stuff in here
+        We override and use onLoad() to access the plugin instance, needed for several features.
+        Because of this overriding, nothing in your plugin's onLoad() will be ran.
+        To fix this, you should override this - onPluginLoad() - instead, and put your stuff in here.
     */ }
 
     final override fun onLoad() {
