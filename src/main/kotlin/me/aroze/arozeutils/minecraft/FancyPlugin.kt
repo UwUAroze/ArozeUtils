45414e321f6fff2f081c4cf16194bb2e0488f415
UwUAroze <admin@aroze.me>
2023-01-29 23:09:27 +0000

    Explained the use of onPluginLoad()

--- src/main/kotlin/me/aroze/arozeutils/minecraft/FancyPlugin.kt
+++ src/main/kotlin/me/aroze/arozeutils/minecraft/FancyPlugin.kt
@@ -9,7 +9,11 @@ private lateinit var pluginInstance: FancyPlugin
 
 abstract class FancyPlugin: JavaPlugin() {
 
-    open fun onPluginLoad() { /*  */ }
+    open fun onPluginLoad() {/*
+        We override and use onLoad() to access the plugin instance, needed for several features
+        Because of this overriding, nothing in your plugin's onLoad() will be run
+        To fix this, you should override this - onPluginLoad() - instead, and put your stuff in here
+    */ }
 
     final override fun onLoad() {
         pluginInstance = this
