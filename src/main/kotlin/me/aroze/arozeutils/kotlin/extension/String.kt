867977d33ce475d52d54fdfcfb6473eaf1828a53
UwUAroze <admin@aroze.me>
2023-03-04 19:59:45 +0000

    Added String#prettify

--- src/main/kotlin/me/aroze/arozeutils/kotlin/extension/String.kt
+++ src/main/kotlin/me/aroze/arozeutils/kotlin/extension/String.kt
@@ -1,5 +1,13 @@
 package me.aroze.arozeutils.kotlin.extension
 
+import java.util.*
+
 fun String.replaceCaseInsensitive(text: String, replacement: String): String {
     return this.replace(Regex("(?i)$text"), replacement)
+}
+
+fun String.prettify(): String {
+    return this.replace(Regex("[-_]"), " ")
+        .split(" ")
+        .joinToString(" ") { ((it.getOrNull(0) ?: "").toString()).uppercase() + it.substring(it.length.coerceAtMost(1)).lowercase() }
 }
