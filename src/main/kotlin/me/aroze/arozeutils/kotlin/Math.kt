cd492d5dd268ff38629cb424c368acf4567849d0
UwUAroze <admin@aroze.me>
2023-04-16 14:25:35 -0400

    Added factorPairs()

--- src/main/kotlin/me/aroze/arozeutils/kotlin/Math.kt
+++ src/main/kotlin/me/aroze/arozeutils/kotlin/Math.kt
@@ -2,6 +2,7 @@ package me.aroze.arozeutils.kotlin
 
 import kotlin.math.pow
 import kotlin.math.round
+import kotlin.math.sqrt
 
 private fun Double.roundTo(places: Int): String {
     val factor = 10.0.pow(places)
@@ -15,4 +16,16 @@ fun Long.makeTimestamp(): String {
     val seconds = (this / 1000.0 % 60).roundTo(2)
     return if (minutes > 0) "$minutes minutes and $seconds seconds"
     else "$seconds seconds"
+}
+
+fun factorPairs(num1: Int, num2: Int): List<Pair<Int, Int>> {
+    val factors = mutableListOf<Pair<Int, Int>>()
+    val minNum = minOf(num1, num2)
+    for (i in 1..sqrt(minNum.toDouble()).toInt()) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            factors.add(Pair(i, num1 / i))
+            factors.add(Pair(i, num2 / i))
+        }
+    }
+    return factors.distinct()
 }
